6e9688131da1db2cedcc2d57ebf05bda11bb1206
hoghug <clind714@gmail.com>
2014-11-07 15:05:25 -0800

    create and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,26 @@
 class ProjectsController < ApplicationController
 
+	def index
+		@project = Project.new
+		@projects = Project.all
+	end
+
+	def create
+		@project = Project.new(project_params)
+		if @project.save
+			redirect_to @project
+		end
+
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :misc_html)
+	end
+
 end
